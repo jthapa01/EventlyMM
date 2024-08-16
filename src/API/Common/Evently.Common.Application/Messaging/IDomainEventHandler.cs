d342c3d0861a4bf5df161b7c052ca4eddb39bc05
jeevan thapa <jeevanthapa@microsoft.com>
2024-08-16 15:51:53 -0400

    Cleanup

--- src/API/Common/Evently.Common.Application/Messaging/IDomainEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using Evently.Common.Domain;
-using MediatR;
-
-namespace Evently.Common.Application.Messaging;
-
-public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
-    where TDomainEvent : IDomainEvent;
